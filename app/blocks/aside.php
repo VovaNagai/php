89d004727dfb0a39be638db877ac2f1b9615a60e
Vova Nagai <ghostfalcon.kr@gmail.com>
2019-03-09 23:05:32 +0200

    create header page

---
+++ app/blocks/aside.php
@@ -0,0 +1,7 @@
+<aside class="col-md-4">
+    <div class="p-3 mb-3 bg-warning rounded">
+        <h4><b>Интересные факты</b></h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus amet consequuntur corporis, ipsum labore neque nesciunt vero voluptate? Aperiam aspernatur corporis dicta fugit placeat quia quo recusandae unde ut vel.</p>
+        <img class="img-thumbnail" src="https://seoquick.com.ua/wp-content/uploads/2017/11/SEO-%D0%9F%D0%A0%D0%9E%D0%94%D0%92%D0%98%D0%96%D0%95%D0%9D%D0%98%D0%95-%D0%91%D0%9B%D0%9E%D0%93%D0%90-%D0%A1%D0%9C%D0%98-%D0%9D%D0%9E%D0%92%D0%9E%D0%A1%D0%A2%D0%9D%D0%9E%D0%93%D0%9E-%D0%A1%D0%90%D0%99%D0%A2%D0%90.png" alt="SEO">
+    </div>
+</aside>
