fa8abcb6e7a8436ce76efc855528ea1f5f321f0b
Vova Nagai <ghostfalcon.kr@gmail.com>
2019-03-12 17:18:34 +0200

    reg ajax

--- app/reg.php
+++ app/reg.php
@@ -14,16 +14,18 @@
         <div class="row">
             <div class="col-md-8 mb-3">
                 <h4>Форма регистрации</h4>
-                <form action="reg/reg.php" method="post">
+                <form action="" method="post">
                     <label for="username">Ваше имя</label>
                     <input type="text" name="username" id="username" class="form-control">
                     <label for="email">Email</label>
                     <input type="text" name="email" id="email" class="form-control">
                     <label for="login">Логин</label>
                     <input type="text" name="login" id="login" class="form-control">
-                    <label for="password">Пароль</label>
-                    <input type="password" name="password" id="password" class="form-control">
-                    <button type="submit" class="btn btn-success mt-5">Зарегистрироваться</button>
+                    <label for="pass">Пароль</label>
+                    <input type="password" name="pass" id="pass" class="form-control">
+
+                    <div class="alert alert-danger mt-4" id="errorBlock"></div>
+                    <button id="reg_user" type="button" class="btn btn-success mt-5">Зарегистрироваться</button>
                 </form>
             </div>
             <?php require_once 'blocks/aside.php';?>
@@ -32,10 +34,31 @@
 </main>
 <?php require_once 'blocks/footer.php';?>
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+    $('#reg_user').click(function () {
+        var name = $('#username').val();
+        var email = $('#email').val();
+        var login = $('#login').val();
+        var pass = $('#pass').val();
 
-<!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>-->
-<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>-->
-<!--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>-->
-<script src="js/my.js"></script>
+        $.ajax({
+            url: 'reg/reg.php',
+            type: 'POST',
+            cache: false,
+            data: {'username' : name, 'email' : email, 'login' : login, 'pass' : pass},
+            dataType: 'html',
+            success: function(data) {
+                if(data == 'Good') {
+                    $('#reg_user').text('Good');
+                    $('#errorBlock').hide();
+                } else {
+                    $('#errorBlock').show();
+                    $('#errorBlock').text(data);
+                }
+            }
+        });
+    });
+</script>
 </body>
 </html>
--- app/reg/reg.php
+++ app/reg/reg.php
@@ -2,30 +2,32 @@
     $username = trim(filter_var($_POST['username'], FILTER_SANITIZE_STRING)); // фильтер опред. переменной, оперд. фильтрации
     $email = trim(filter_var($_POST['email'], FILTER_SANITIZE_EMAIL));
     $login = trim(filter_var($_POST['login'], FILTER_SANITIZE_STRING));
-    $password = trim(filter_var($_POST['password'], FILTER_SANITIZE_STRING));
+    $pass = trim(filter_var($_POST['pass'], FILTER_SANITIZE_STRING));
 
-    // проверка на ошибки, пока без ajax
-
+    // проверка на ошибки
+    $error = '';
     if(strlen($username) <= 3)
-        exit();
+        $error = 'Введите имя';
     else if (strlen($email) <= 3)
-        exit();
+        $error = 'Введите email';
      else if (strlen($login) <= 3)
-        exit();
-    else if (strlen($password) <= 3)
-        exit();
+         $error = 'Введите логин';
+    else if (strlen($pass) <= 3)
+        $error = 'Введите пароль';
 
-    $hash = "d6f6gf345hhgr3etwer42ferg43s";
-    $password = md5($password . $hash); // ф-ция для шифровки пароля
+    if ($error != '') {
+        echo $error;
+        exit();
+    }
 
-    //$user = 'root';
-    //$password = 'root';
+    $pass = md5($pass); // ф-ция для шифровки пароля
+
     $db = 'phpblog';
     $host = 'localhost'; // данные для локального сервера
 
     /* Подключение к базе данных MySQL с помощью вызова драйвера */
 	$dsn = 'mysql:dbname=phpblog;host=127.0.0.1';
-	$user = 'falcon';
+	$user = 'root';
 	$password = '';
 
 	try {
@@ -34,9 +36,9 @@
 	    echo 'Подключение не удалось: ' . $e->getMessage();
 	}
 
-	var_dump($pdo);
-    $sql = 'INSERT INTO users(name, email, login, password) VALUES(?, ?, ?, ?)';
-    $query = $pdo->prepare($sql);
 
-	var_dump($query);
-    $query->execute([$username, $email, $login, $password]);
+    $sql = 'INSERT INTO users(name, email, login, pass) VALUES(?, ?, ?, ?)';
+    $query = $pdo->prepare($sql);
+    $query->execute([$username, $email, $login, $pass]);
+
+    echo 'Good';
